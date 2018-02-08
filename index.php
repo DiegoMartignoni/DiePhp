726674ea5a9b1c6212e4ce5eb6025981c570313e
Martignoni Diego <martignoni.15445@PALEOCAPA.DID>
2018-02-08 11:46:10 +0100

    nuovo index, inserimento titolo e corpo, form action su lista.php di altervista

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en"class="w-100 h-100">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Foglio Accessi</title>
+  </head>
+  <body class="w-100 h-100">
+    <div class="jumbotron mb-0 d-flex w-100 h-100 justify-content-center">
+      <div class="align-self-center">
+        <h1><a class="badge badge-warning align-self-center" href=".">+</a> Lista</h1>
+        <form class="form-group" action="http://diegomartignoni.altervista.org/heroku/diephp/lista.php" method="get">
+          <input type="text" name="titolo" class="form-control mb-2" placeholder="Titolo" required autofocus>
+          <input type="text" name="corpo" class="form-control mb-2" placeholder="Corpo" required>
+          <button class="btn btn-lg btn-warning btn-block" type="submit">Salva</button>
+        </form>
+      </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
